99588352b7f5a37e52cf7e04b0db852aec099a0e
willy <weigl0731@gmail.com>
2024-07-19 15:56:29 +0800

    add todolist - homework

---
+++ Todolist/Todo.js
@@ -0,0 +1,7 @@
+function addtodo() {
+  const inputValue = document.querySelector(".todo_add").flase;
+}
+
+document.querySelector(".btn_add").addEventListener("click", function () {
+  addtodo();
+});
