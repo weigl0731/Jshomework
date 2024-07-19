423a58ab3647783d510c56b5db705f35c0628dc7
WILLYD\weigl <weigl0731@gmail.com>
2024-07-20 00:02:43 +0800

    updata

--- Todolist/Todo.js
+++ Todolist/Todo.js
@@ -4,3 +4,17 @@ let btn_add = document.querySelector(".btn_add");
 let btnground = document.querySelector(".btnground");
 let btndel = document.querySelector("btndel");
 let btnedit = document.querySelector("btnedit");
+let TodoList = document.querySelector("toDOList");
+
+btn_add.addEventListener("click", function () {
+  let inputvalue = todo_add.value;
+  addItem(inputvalue);
+});
+
+function addItem() {
+  let newItem = document.createElement("li");
+  newItem.innerHTML = `<input type="" / >
+  <input class="btndel" type="button" value="del" />
+  <input class="btnedit" type="button" value="edit" />`;
+  TodoList.appendChild(newItem);
+}
