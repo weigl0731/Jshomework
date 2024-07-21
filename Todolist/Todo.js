919bef135c2cab23f1bfb51ed83fea681458a418
willy <weigl0731@gmail.com>
2024-07-21 11:31:13 +0800

    todo list add function run ok

--- Todolist/Todo.js
+++ Todolist/Todo.js
@@ -2,19 +2,30 @@ let todo_add = document.querySelector(".todo_add");
 let btn_add = document.querySelector(".btn_add");
 
 let btnground = document.querySelector(".btnground");
-let btndel = document.querySelector("btndel");
-let btnedit = document.querySelector("btnedit");
-let TodoList = document.querySelector("toDOList");
+let btndel = document.querySelector(".btndel");
+let btnedit = document.querySelector(".btnedit");
+let TodoList = document.querySelector(".toDoList");
 
 btn_add.addEventListener("click", function () {
   let inputvalue = todo_add.value;
   addItem(inputvalue);
+  // console.log(inputvalue);
 });
 
-function addItem() {
+let count = 0;
+
+function addItem(x) {
   let newItem = document.createElement("li");
-  newItem.innerHTML = `<input type="" / >
+  newItem.id = `item-${count}`;
+  count++;
+  // newItem.addClass = btnground;
+
+  newItem.innerHTML = `<input type="" value = "${x}" />
   <input class="btndel" type="button" value="del" />
   <input class="btnedit" type="button" value="edit" />`;
-  TodoList.appendChild(newItem);
+  TodoList.append(newItem);
+}
+
+function delItem(a) {
+  let delitem = document.querySelector();
 }
