8bf9ca221931c0669faee9ceffa55bc591d580ac
WILLYD\weigl <weigl0731@gmail.com>
2024-07-22 15:27:33 +0800

    updata code

--- js Basics training/script3-4.js
+++ js Basics training/script3-4.js
@@ -39,18 +39,63 @@
 
 // let arr = []; // 宣告
 // for (let i = 0; i < 5; i++) {
-//   arr[i] = Math.trunc(Math.random() * 10); // 設定值
+//   arr[i] = Math.floor(Math.random() * 10);
+
+//   for (let j = 0; j < i; j++) {
+//     console.log("arr[i]等於 :" + arr[i] + "\t arr[j]等於 :" + arr[j]);
+//     if (arr[i] == arr[j]) {
+//       i--;
+//       console.log("陣列數字重複");
+//     }
+//   }
+//   console.log(arr + "\n");
 // }
 // console.log(arr);
-
-// -------------- B   ---------------- ok
+// -------------- B   ----------------  ok
 
 let arr = []; // 宣告
 let sum = 0;
-let average = "";
+let average = 0;
 for (let i = 0; i < 5; i++) {
-  arr[i] = Math.trunc(Math.random() * 10); // 設定值
-  sum += arr[i];
+  arr[i] = Math.floor(Math.random() * 10);
+
+  for (let j = 0; j < i; j++) {
+    console.log("arr[i]等於 :" + arr[i] + "\t arr[j]等於 :" + arr[j]);
+    if (arr[i] == arr[j]) {
+      i--;
+      console.log("陣列數字重複");
+    }
+  }
+  
 }
 
-console.log(sum);
+console.log("陣列裡的數字為 :\n" + arr + "\n");
+sum += arr;
+
+average += Math.floor(sum / arr.length);
+
+console.log("陣列數字的總和為:\t" + sum + "\n陣列數字的平均為:\t" + average);
+
+
+
+//  -----------------   C  ---------------------
+
+// let arr = [];
+// let sum = 0;
+// let average = 0;
+// for (let i = 0; i < 5; i++) {
+//   arr[i] = Math.trunc(Math.random() * 10);
+//   sum += arr[i];
+// }
+// console.log("陣列裡的數字為:");
+// console.log(arr.sort() + "\n");
+// average += Math.trunc(sum / arr.length);
+
+// console.log("陣列數字的平均為:\t" + average);
+
+// if (arr.indexOf(average) !== -1) {
+//   console.log("陣列元素的值等於平均值的數字:" + average);
+// } else if (arr.indexOf(average) == -1) {
+//   a = arr.indexOf(average);
+
+// }
