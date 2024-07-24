d25b5075bedcc23f530ee678f5b51a2395787bb8
WILLYD\weigl <weigl0731@gmail.com>
2024-07-24 08:07:44 +0800

    updata code

--- js homework1/script1-2.js
+++ js homework1/script1-2.js
@@ -1,77 +1,78 @@
-// 1.
+1;
 
 // ------------------------a.----------------------ok
-// console.log(
-//   "int " +
-//     "while " +
-//     "break " +
-//     "for " +
-//     "if " +
-//     "switch " +
-//     "else " +
-//     "case " +
-//     "continue " +
-//     "foreach "
-// );
+console.log(
+  "int " +
+    "while " +
+    "break " +
+    "for " +
+    "if " +
+    "switch " +
+    "else " +
+    "case " +
+    "continue " +
+    "foreach "
+);
 // ------------b.-----------------------ok
 
-// const pi = 3.14;
+const pi = 3.14;
+console.log(pi);
 
 // -----------------c.--------------------ok
-// let name1 = "willy";
-// let age = 26;
+let name1 = "willy";
+let age = 26;
 
-// console.log("我的名字叫 " + name1 + " 年紀為 " + age);
+console.log("我的名字叫 " + name1 + " 年紀為 " + age);
 
-// 2.
+2;
 
 // -----------------------a + b  ---------------------ok
-// let dayhrsec = 24 * 60 * 60;
-// let hoursec = 60 * 60;
-// let minuteSec = 60;
-// let Sec_1 = 36;
-// a = 1 * dayhrsec + 10 * hoursec + 17 * minuteSec + Sec_1;
+let dayhrsec = 24 * 60 * 60;
+let hoursec = 60 * 60;
+let minuteSec = 60;
+let Sec_1 = 36;
+a = 1 * dayhrsec + 10 * hoursec + 17 * minuteSec + Sec_1;
 
-// console.log("1 天又 10 小時又 17 分又 36 秒的秒數等於 = " + a + "秒");
+console.log("1 天又 10 小時又 17 分又 36 秒的秒數等於 = " + a + "秒");
 
-// let secDay = Math.floor(93784 / dayhrsec);
-// let secHour = Math.floor((93784 % dayhrsec) / hoursec);
-// let secMinute = Math.floor(((93784 % dayhrsec) % hoursec) / minuteSec);
-// let Sec_2 = Math.floor(((93784 % dayhrsec) % hoursec) % minuteSec);
-// console.log(
-//   "93784秒 = " +
-//     secDay +
-//     "天" +
-//     secHour +
-//     "小時" +
-//     secMinute +
-//     "分" +
-//     Sec_2 +
-//     "秒"
-// );
+let secDay = Math.floor(93784 / dayhrsec);
+let secHour = Math.floor((93784 % dayhrsec) / hoursec);
+let secMinute = Math.floor(((93784 % dayhrsec) % hoursec) / minuteSec);
+let Sec_2 = Math.floor(((93784 % dayhrsec) % hoursec) % minuteSec);
+console.log(
+  "93784秒 = " +
+    secDay +
+    "天" +
+    secHour +
+    "小時" +
+    secMinute +
+    "分" +
+    Sec_2 +
+    "秒"
+);
 
 //  -------------------------c.----------------------ok
 
-// let havcom = "如果有筆電且有網路，";
-// let homwrok = "就可以在家工作";
+let havcom = "如果有筆電且有網路，";
+let homwrok = "就可以在家工作";
 
-// havhomwrok = havcom + homwrok;
+havhomwrok = havcom + homwrok;
 
-// console.log(havhomwrok);
+console.log(havhomwrok);
 
 //   ---------------------d--------------- ok
 
-// let openComputer = 1;
-// let openvscode = 2;
-// let writehomework = 3;
+let openComputer = 1;
+let openvscode = 2;
+let writehomework = 3;
 
-// oneday = openComputer + openvscode + writehomework * 3;
-// twoday = openComputer + openvscode + writehomework * 2;
-// a = oneday + twoday;
+oneday = openComputer + openvscode + writehomework * 3;
+twoday = openComputer + openvscode + writehomework * 2;
+a = oneday + twoday;
 
-// console.log("Reds 2天總共花了 : " + a + "分鐘");
+console.log("Reds 2天總共花了 : " + a + "分鐘");
 
 //  ------------------------e.---------------------- ok
 
-// let n = 10;
-// console.log(Math.trunc(Math.random() * n));
+let n = Math.trunc(Math.random() * 10) + 1;
+console.log(n);
--- js homework1/script3-4.js
+++ js homework1/script3-4.js
@@ -2,230 +2,284 @@
 
 // --------------------a --------------------ok
 
-// let n = 10;
-// let total = 3;
-// let total2 = 4;
+let n = Math.trunc(Math.random() * 10) + 1;
+let total = 3;
+let total2 = 4;
 
-// for (let i = 1; i <= n; i++) {
-//   total *= 2;
-//   total2 *= 2;
-// }
-// console.log("3的倍數總和" + total + "\t4的倍數總和" + total2);
+for (let i = 1; i <= n; i++) {
+  total *= 2;
+  total2 *= 2;
+}
+
+console.log(`1~n的數為${n} 3的倍數總和${total} 4的倍數總和${total2}`);
 
 //  ---------------  B   --------------- ok
 
-// let a = 9;
-// let str = "";
+let a = 9;
+let str = "";
 
-// for (let i = 1; i <= 8; i++) {
-//   let count = 2;
-//   for (let j = 1; j <= a - 1; j++) {
-//     b = a * count;
-//     count++;
-//     str += b + "\t";
-//   }
-//   a--;
-//   str += "\n";
-// }
-// console.log(str);
+for (let i = 1; i <= 8; i++) {
+  let count = 2;
+  for (let j = 1; j <= a - 1; j++) {
+    b = a * count;
+    count++;
+    str += b + "\t";
+  }
+  a--;
+  str += "\n";
+}
+console.log(str);
 
 // -------------------------------------
 
 //   --------   C  -------  ok
 
-// leit str = ""
-// for(let i = 0 ; i <1 ; i++){
-//   let a = -1
-//   for (let j =1; j <= 11 ; j++ ){
-//     a += 3
-//     str += a + "\t";
-//   }
-// }
-// console.log(str);
+let str1 = "";
+for (let i = 0; i < 1; i++) {
+  let a = -1;
+  for (let j = 1; j <= 11; j++) {
+    a += 3;
+    str1 += a + "\t";
+  }
+}
+console.log(str1);
 
-// ----------- D --------------
-// let n = 10;
-// let sum = 0;
+// ----------- D --------------   ok
+let nn = Math.trunc(Math.random() * 50) + 1;
+let sum = 0;
+function isprime(a) {
+  if (a == 1) {
+    return false;
+  }
+  for (let i = 2; i < a; i++) {
+    if (a % i == 0) {
+      return false;
+    }
+    return true;
+  }
+}
 
-// function isprime(a) {
-//   if (a === 1) {
-//     return false;
-//   }
-//   for (let i = 2; i < a; i++) {
-//     if (a % i === 0) {
-//       return false; // 能整除i所以不是質數
-//     }
-//     return true;
-//   }
-// }
-
-// for (let b = 2; b < n; b++) {
-//   if (isprime(n)) {
-//     sum += b;
-//   }
-//   console.log(b);
-// }
-
-// console.log(sum);
+for (let b = 2; b < n; b++) {
+  if (isprime(nn)) {
+    sum += b;
+  }
+}
+console.log(`質數${nn}`);
+console.log(sum);
 
 // ------------- E -----------------  ok
 
-// function getDaysInMonth(year, month) {
-//   if ((year % 4 == 0 && year % 100 !== 0) || year % 400 === 0) {
-//     switch (month) {
-//       case 1:
-//       case 3:
-//       case 5:
-//       case 7:
-//       case 8:
-//       case 10:
-//       case 12:
-//         return 31;
-//       case 4:
-//       case 6:
-//       case 9:
-//       case 11:
-//         return 30;
-//       case 2:
-//         return 29;
-//     }
-//   } else {
-//     switch (month) {
-//       case 1:
-//       case 3:
-//       case 5:
-//       case 7:
-//       case 8:
-//       case 10:
-//       case 12:
-//         return 31;
-//       case 4:
-//       case 6:
-//       case 9:
-//       case 11:
-//         return 30;
-//       case 2:
-//         return 28;
-//     }
-//   }
-// }
-// year = 2023;
-// month = 2;
-// a = getDaysInMonth(year, month);
-// console.log("2023年的2月份有幾天:\t" + a + "天");
+function getDaysInMonth(year, month) {
+  if ((year % 4 == 0 && year % 100 !== 0) || year % 400 === 0) {
+    switch (month) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        return 31;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        return 30;
+      case 2:
+        return 29;
+    }
+  } else {
+    switch (month) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        return 31;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        return 30;
+      case 2:
+        return 28;
+    }
+  }
+}
+year = 2023;
+month = 2;
+a = getDaysInMonth(year, month);
+console.log("2023年的2月份有幾天:\t" + a + "天");
 
 /*   ------------------------------    4.     ----------------------- */
 
 // -------------- A   ----------------  ok
 
-// let arr = []; // 宣告
-// for (let i = 0; i < 5; i++) {
-//   arr[i] = Math.floor(Math.random() * 10);
-
-//   for (let j = 0; j < i; j++) {
-//     console.log("arr[i]等於 :" + arr[i] + "\t arr[j]等於 :" + arr[j]);
-//     if (arr[i] == arr[j]) {
-//       i--;
-//       console.log("陣列數字重複");
-//     }
-//   }
-//   console.log(arr + "\n");
-// }
-// console.log(arr);
-// -------------- B   ----------------  ok
-
-// let arr = []; // 宣告
-// let sum = 0;
-// let average = 0;
-// for (let i = 0; i < 5; i++) {
-//   arr[i] = Math.floor(Math.random() * 10);
-//   for (let j = 0; j < i; j++) {
-//     console.log("arr[i]等於 :" + arr[i] + "\t arr[j]等於 :" + arr[j]);
-//     if (arr[i] == arr[j]) {
-//       i--;
-//       console.log("陣列數字重複");
-//     }
-//   }
-// }
-// for (let a of arr) {
-//   sum += a;
-// }
-
-// console.log("陣列裡的數字為 :\n" + arr + "\n");
-// average += Math.floor(sum / arr.length);
-
-// console.log("陣列數字的總和為:\t" + sum + "\n陣列數字的平均為:\t" + average);
-
-//  -----------------   C  ---------------------
-
 let arr = []; // 宣告
-let sum = 0;
-let average = 0;
 for (let i = 0; i < 5; i++) {
   arr[i] = Math.floor(Math.random() * 10);
+
   for (let j = 0; j < i; j++) {
+    console.log("arr[i]等於 :" + arr[i] + "\t arr[j]等於 :" + arr[j]);
     if (arr[i] == arr[j]) {
+      i--;
+      console.log("陣列數字重複");
+    }
+  }
+  console.log(arr + "\n");
+}
+console.log(arr);
+// -------------- B   ----------------  ok
+
+let arr1 = []; // 宣告
+let sum1 = 0;
+let average = 0;
+for (let i = 0; i < 5; i++) {
+  arr1[i] = Math.floor(Math.random() * 10);
+  for (let j = 0; j < i; j++) {
+    console.log("arr1[i]等於 :" + arr1[i] + "\t arr1[j]等於 :" + arr1[j]);
+    if (arr1[i] == arr1[j]) {
+      i--;
+      console.log("陣列數字重複");
+    }
+  }
+}
+for (let a of arr1) {
+  sum += a;
+}
+
+console.log("陣列裡的數字為 :\n" + arr1 + "\n");
+average += Math.floor(sum1 / arr1.length);
+
+console.log("陣列數字的總和為:\t" + sum1 + "\n陣列數字的平均為:\t" + average);
+
+//  -----------------   C  --------------------- ok
+
+let arr2 = []; // 宣告
+let sum2 = 0;
+let average1 = 0;
+for (let i = 0; i < 5; i++) {
+  arr2[i] = Math.floor(Math.random() * 10);
+  for (let j = 0; j < i; j++) {
+    if (arr2[i] == arr2[j]) {
       i--;
     }
   }
 }
-for (let a of arr) {
-  sum += a;
+for (let a of arr2) {
+  sum2 += a;
 }
 
-console.log(arr); // 排序前
-arr = arr.sort(); //排序後
-console.log(arr);
-average += Math.floor(sum / arr.length);
-console.log("平均數 = " + average);
-if (arr.indexOf(average) !== -1) {
-  console.log(
-    "陣列元素之值等於平均值 = " + "第" + arr.indexOf(average) + "個數"
-  );
-} else if (arr.indexOf(average) == -1) {
-  for (let i = 0; i < 5; i++) {
-    if (average > arr[i]) {
-      console.log();
-    }
+console.log(arr2); // 排序前
+arr2 = arr2.sort(); //排序後
+console.log(arr2);
+average1 += Math.floor(sum2 / arr2.length);
+console.log("平均數 = " + average1);
+for (let i = 0; i < arr2.length; i++) {
+  if (arr2[i] == average1) {
+    console.log(`陣列元素之值等於平均值 = ${i}`);
+  } else if (arr2[i] < average1 && arr2[i + 1] > average1) {
+    console.log(`平均值介於哪兩個元素值之間的數為${i}和${i + 1}`);
   }
 }
 
-// for (let i = 0; i < 5; i++) {
-//   z = arr.indexOf(average);
-//   console.log(z);
-// }
+//------------------ D ---------------  ok
 
-// if(d.length)
+const mySkills = ["javascript", "java", "sql", "c", "python", "php", "swift"];
+let mySkillsstr = mySkills.join("");
+let jcount = 0;
+let acount = 0;
+let vcount = 0;
+let scount = 0;
+let ccount = 0;
+let rcount = 0;
+let icount = 0;
+let pcount = 0;
+let tcount = 0;
+let qcount = 0;
+let lcount = 0;
+let ycount = 0;
+let hcount = 0;
+let ocount = 0;
+let ncount = 0;
+let wcount = 0;
+let fcount = 0;
 
-//------------------ D ---------------
+for (let i = 0; i < mySkillsstr.length; i++) {
+  if (mySkillsstr[i] == "j") {
+    jcount += 1;
+  }
+  if (mySkillsstr[i] == "a") {
+    acount += 1;
+  }
+  if (mySkillsstr[i] == "v") {
+    vcount += 1;
+  }
+  if (mySkillsstr[i] == "s") {
+    scount += 1;
+  }
+  if (mySkillsstr[i] == "c") {
+    ccount += 1;
+  }
+  if (mySkillsstr[i] == "r") {
+    rcount += 1;
+  }
+  if (mySkillsstr[i] == "i") {
+    icount += 1;
+  }
+  if (mySkillsstr[i] == "p") {
+    pcount += 1;
+  }
+  if (mySkillsstr[i] == "t") {
+    tcount += 1;
+  }
+  if (mySkillsstr[i] == "q") {
+    qcount += 1;
+  }
+  if (mySkillsstr[i] == "l") {
+    lcount += 1;
+  }
+  if (mySkillsstr[i] == "y") {
+    ycount += 1;
+  }
+  if (mySkillsstr[i] == "h") {
+    hcount += 1;
+  }
+  if (mySkillsstr[i] == "o") {
+    ocount += 1;
+  }
+  if (mySkillsstr[i] == "n") {
+    ncount += 1;
+  }
+  if (mySkillsstr[i] == "w") {
+    wcount += 1;
+  }
+  if (mySkillsstr[i] == "f") {
+    fcount += 1;
+  }
+}
+console.log(
+  ` j的總數為${jcount}\n a的總數為${acount}\n v的總數為${vcount}\n s的總數為${scount}\n c的總數為${ccount}\n r的總數為${rcount}\n i的總數為${icount}\n p的總數為${pcount}
+ t的總數為${tcount}\n q的總數為${qcount}\n l的總數為${lcount}\n y的總數為${ycount}\n h的總數為${hcount}\n o的總數為${ocount}\n n的總數為${ncount}\n w的總數為${wcount}
+ f的總數為${fcount}`
+);
 
-// const mySkills = ["javascript", "java", "sql", "c", "python", "php", "swift"];
+// --------------- E ----------------------  ok
 
-// function indexcount(x, char) {
-//   let count = 0;
-//   let index = x.indexOf(char);
-//   while (index !== -1) {
-//     count++;
-//     index = x.indexOf(char, index + 1);
-//   }
-//   return count;
-// }
-// let jCount = indexcount(mySkills[], "j");
-// console.log("j有:" + jCount);
+let arr3 = [];
+let a3 = 9;
 
-// --------------- E ----------------------
-
-// let arr = [];
-// let a = 9;
-// for (let i = 0; i <= 8; i++) {
-//   let count = 2;
-//   let arr2 = [];
-//   for (let j = 1; j <= a - 1; j++) {
-//     arr2[j] = a * count;
-
-//     count++;
-//   }
-//   a--;
-// }
-// console.log(arr2);
-// console.log(arr);
+for (let i = 1; i <= 8; i++) {
+  let arr4 = [];
+  let count = 2;
+  for (let j = 1; j <= a3 - 1; j++) {
+    b = a3 * count;
+    count++;
+    arr4.push(b);
+  }
+  a3--;
+  arr3.push(arr4);
+}
+console.log(arr3);
